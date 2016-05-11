247551dd20d8634631074541684cb5449cc2a6e3
Pedro Ferreira <plferreira4@gmail.com>
2016-05-11 23:48:43 +0100

    - add a 'Map' class to deal with all map related actions (playing and drawing the map) - fix, make sure the column/line values used are 0-based (only the user input is 1-based)

--- main.swift
+++ main.swift
@@ -1,18 +1,14 @@
 import Glibc
 
-let map = [
-    [ 0, 0, 0 ],
-    [ 0, 0, 0 ],
-    [ 0, 0, 0 ],
-]
 
 func start()
 {
 print( "Hello there!" )
+let map = Map()
 
 while true
     {
-    draw()
+    map.draw()
     print( "Write the line and column you want to play (for example: \"1 3\" - first line and third column)." )
     let input = readLine( stripNewline: true )
 
@@ -22,10 +18,12 @@ while true
         }
 
     var (column, line) = getPlayerMove( input! )
-    play( column, line )
+
+        // user inputs values from 1 to 3, but we work in 0-based values
+    map.play( column - 1, line - 1 )
 
     (column, line) = getBotMove()
-    play( column, line )
+    map.play( column, line )
 
     if ( checkGameEnd() )
         {
@@ -47,49 +45,6 @@ func restart()
 }
 
 
-func draw()
-{
-for line in 0 ..< map.count
-    {
-    let lineArray = map[ line ]
-
-    for column in 0 ..< lineArray.count
-        {
-        let value = lineArray[ column ]
-        var valueSymbol = " "
-
-        if value < 0
-            {
-            valueSymbol = "X"
-            }
-
-        else if value > 0
-            {
-            valueSymbol = "O"
-            }
-
-        print( valueSymbol, terminator: "" )
-
-        if column + 1 != lineArray.count
-            {
-            print( "#", terminator: "" )
-            }
-
-        else
-            {
-            print()
-            }
-        }
-
-        // if not the last element
-    if line + 1 != map.count
-        {
-        print( "#####" )
-        }
-    }
-}
-
-
 func getPlayerMove( input: String ) -> (Int, Int)
 {
 let values = input.characters.split{ $0 == " " }.map( String.init )
@@ -103,28 +58,88 @@ return (column, line)
 
 func getBotMove() -> (Int, Int)
 {
-let column = getRandomInt( 1, 3 )
-let line = getRandomInt( 1, 3 )
+let column = getRandomInt( 0, 2 )
+let line = getRandomInt( 0, 2 )
 
 return (column, line)
-}
-
-
-func play( column: Int, _ line: Int )
-{
-if column < 1 || column > 3 ||
-   line   < 1 || line   > 3
-    {
-    return
-    }
-
-print( "Played \(line) \(column) line/column." )
 }
 
 
 func getRandomInt( min: Int, _ max: Int ) -> Int
 {
 return Int( random() % (max - min + 1) ) + min
+}
+
+
+class Map
+{
+var MAP = [
+        [ 0, 0, 0 ],
+        [ 0, 0, 0 ],
+        [ 0, 0, 0 ],
+    ]
+
+func draw()
+    {
+    for line in 0 ..< MAP.count
+        {
+        let lineArray = MAP[ line ]
+
+        for column in 0 ..< lineArray.count
+            {
+            let value = lineArray[ column ]
+            var valueSymbol = " "
+
+            if value < 0
+                {
+                valueSymbol = "X"
+                }
+
+            else if value > 0
+                {
+                valueSymbol = "O"
+                }
+
+            print( valueSymbol, terminator: "" )
+
+            if column + 1 != lineArray.count
+                {
+                print( "#", terminator: "" )
+                }
+
+            else
+                {
+                print()
+                }
+            }
+
+            // if not the last element
+        if line + 1 != MAP.count
+            {
+            print( "#####" )
+            }
+        }
+    }
+
+func play( column: Int, _ line: Int )
+    {
+    if column < 0 || column > 2 ||
+       line   < 0 || line   > 2
+        {
+        return
+        }
+
+    if MAP[ line ][ column ] == 0
+        {
+        MAP[ line ][ column ] = 1
+        print( "Played \(line + 1) \(column + 1) line/column." )
+        }
+
+    else
+        {
+        print( "Position already taken." )
+        }
+    }
 }
 
 

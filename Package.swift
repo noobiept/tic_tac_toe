47e4db5f229c606831c387a0c55915a0f0a04db5
Pedro Ferreira <plferreira4@gmail.com>
2018-01-17 21:22:50 +0000

    - start using swift v4.0 - update the package file to the newer format

--- Package.swift
+++ Package.swift
@@ -1,7 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
-
 
 let package = Package(
     name: "tic_tac_toe",
-    dependencies: []
+    products: [
+        .executable( name: "tic_tac_toe", targets: [ "tic_tac_toe" ] )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "tic_tac_toe",
+            dependencies: [],
+            path: "./Sources"
+        )
+    ]
 )
